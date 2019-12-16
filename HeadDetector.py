7251279d517c39c5e5b49d2473e930e291702a67
Panusin <panusin.dodo@mail.kmutt.ac.th>
2019-12-16 13:53:16 +0700

    Update HeadDetector.py

--- HeadDetector.py
+++ HeadDetector.py
@@ -1,18 +1,3 @@
-# PyKinect
-# Copyright(c) Microsoft Corporation
-# All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the License); you may not use
-# this file except in compliance with the License. You may obtain a copy of the
-# License at http://www.apache.org/licenses/LICENSE-2.0
-#
-# THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
-# OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY
-# IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
-# MERCHANTABLITY OR NON-INFRINGEMENT.
-#
-# See the Apache Version 2.0 License for specific language governing
-# permissions and limitations under the License.
 import itertools
 import ctypes
 from pykinect import nui
@@ -195,7 +180,6 @@ if __name__ == '__main__':
     PORT = 60000  # Port to listen on (non-privileged ports are > 1023)
     thread1 = threading.Thread(target=work1)
     thread2 = threading.Thread(target=work2)
-
     thread1.start()
     thread2.start()
     # ----------------------------------------------------------------------
